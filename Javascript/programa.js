4d1c79fa61e0f0b690084426ec2cc73d59bab6c4
UserAI678 <138083898+UserAI678@users.noreply.github.com>
2024-07-05 19:25:30 -0300

    cambios en el codigo parte 1

--- Javascript/programa.js
+++ Javascript/programa.js
@@ -1,6 +1,14 @@
-var nombre_px = []
-var cantidad_px = []
+let nombre_px_inp = document.getElementById("nombre_px")
+let cantidad_px_inp = document.getElementById("cant_px")
+let precio_px_inp = document.getElementById("precio_px")
 
-function inventario(){
+function crear_producto(){
+    var nombre_px = []
+    var cantidad_px = []
+    var precio_px = []
+}
+
+
+function eliminar_producto(){
 
 }
