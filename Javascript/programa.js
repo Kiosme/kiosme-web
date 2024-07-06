d3f883273cb2bab82592693b979a837d8c3aa343
UserAI678 <138083898+UserAI678@users.noreply.github.com>
2024-07-05 22:04:41 -0300

    Cambios en el proyecto part 2

--- Javascript/programa.js
+++ Javascript/programa.js
@@ -2,10 +2,8 @@ let nombre_px_inp = document.getElementById("nombre_px")
 let cantidad_px_inp = document.getElementById("cant_px")
 let precio_px_inp = document.getElementById("precio_px")
 
-function crear_producto(){
-    var nombre_px = []
-    var cantidad_px = []
-    var precio_px = []
+function nombre_px (){
+    
 }
 
 
