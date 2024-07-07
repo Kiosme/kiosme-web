838c2ce5e7df494997cc016311cff4187acc2ff0
UserAI678 <138083898+UserAI678@users.noreply.github.com>
2024-07-06 21:55:23 -0300

    cambio en la inventario parte 1

--- Javascript/programa.js
+++ Javascript/programa.js
@@ -2,11 +2,6 @@ let nombre_px_inp = document.getElementById("nombre_px")
 let cantidad_px_inp = document.getElementById("cant_px")
 let precio_px_inp = document.getElementById("precio_px")
 
-function nombre_px (){
+let crear_px  = () => {
     
-}
-
-
-function eliminar_producto(){
-
 }
