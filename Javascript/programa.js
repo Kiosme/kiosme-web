584acf21f2d0d38ceca47582b2e82375d4a52557
UserAI678 <138083898+UserAI678@users.noreply.github.com>
2024-07-09 01:40:27 -0300

    func. de Inventario solo falta DB y edit, borrar

--- Javascript/programa.js
+++ Javascript/programa.js
@@ -1,7 +1,32 @@
-let nombre_px_inp = document.getElementById("nombre_px")
-let cantidad_px_inp = document.getElementById("cant_px")
-let precio_px_inp = document.getElementById("precio_px")
 
-let crear_px  = () => {
-    
+var id = 0;
+
+function crear_px(){
+    id += 1
+    //las variables que se concectan con la interfaz
+    var crear_tbl = document.getElementById("add_tl");
+    let nombre_px_inp = document.getElementById("nombre_px").value;
+    let cantidad_px_inp = document.getElementById("cant_px").value;
+
+    //donde va la creacion de tablas
+
+    var new_row = crear_tbl.insertRow(crear_tbl.rows.length);
+    new_row.insertCell(0).innerHTML = id;
+    new_row.insertCell(1).innerHTML = nombre_px_inp;
+    new_row.insertCell(2).innerHTML = cantidad_px_inp;
+    //guardado en localstorage
+    localStorage.setItem("id",id)
+    localStorage.setItem("nombre_px_inp",nombre_px_inp)
+    localStorage.setItem("cantidad_px_inp",cantidad_px_inp)
 }
+
+
+function esconder_mod_menu(){}
+
+function editar_px(){}
+
+
+function borrar(){}
+
+let data = [{}];
+
